7734c2927b0fb68d666889c474bb63872901255f
Maged Groups <magedyaseengroups@gmail.com>
2026-02-08 22:06:36 +0200

    feat: add the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,14 @@ export default function App() {
             <Header />
             
             <Main />
-            
+            <aside>
+                <h2>Side bar content</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur ipsam dolorem maiores corporis temporibus itaque? Cum accusantium, exercitationem odio vero quos aspernatur iste impedit ad, saepe est assumenda repellat tenetur.</p>
+                <h4>Section A Title</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, doloremque.</p>
+                <h4>Section B Title</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, doloremque.</p>
+            </aside>
             <Footer />
         </>
     )
